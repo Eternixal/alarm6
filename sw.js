ad5250176128f58c93dde4543bd64054d8c8c4b7
Eternixal <adamristian@gmail.com>
2026-01-30 23:16:09 +0700

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = 'nerdix-v1';
-const ASSETS = ['./'];
+const CACHE_NAME = 'sentinel-v6';
+const assets = [
+  './', 
+  './index.html', 
+  './manifest.json',
+  './nerdix.jpg', // Ganti dengan nama file gambar kamu
+  './nerdix-bg.jpg'  // Contoh jika ada gambar latar belakang
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
